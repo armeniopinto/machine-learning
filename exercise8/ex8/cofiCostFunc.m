c740c3f080a47a0042186b324200c55d7ade168b
Armenio Pinto <armeniopinto@gmail.com>
2016-10-14 22:34:48 +0100

    Completed section 5 of exercise 8.

--- exercise8/ex8/cofiCostFunc.m
+++ exercise8/ex8/cofiCostFunc.m
@@ -45,6 +45,8 @@ X_grad = ((X * Theta' - Y) .* R) * Theta;
 Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
 J = J + lambda * sum(sum(Theta .^ 2)) / 2 + lambda * sum(sum(X .^ 2)) / 2;
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
 
 % =============================================================
 
